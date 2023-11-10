a140fffebc214044388e1f3dd644ba63399eb57d
SAIRAM <147367113+sairammr@users.noreply.github.com>
2023-11-10 23:45:35 +0530

    Revert "OCR applied version - initial"

--- cashino/lib/main.dart
+++ cashino/lib/main.dart
@@ -1,47 +1,19 @@
 import 'package:cashino/pages/homepage.dart';
 import 'package:flutter/material.dart';
-import 'package:cashino/pages/activitypage.dart';
-import 'package:cashino/pages/addpage.dart';
-import 'package:cashino/pages/learnpage.dart';
-import 'package:cashino/components/navbar.dart';
-
 
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-   MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int currentIndex = 0 ;
-
-  final pages =[
-    homepage(),
-    addpage(),
-     activity(),
-     learnpage()
-  ];
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: pages[currentIndex],
-bottomNavigationBar: CurvedNavigationBarWidget(
-        onIndexChanged: (int index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-)
-      )
+      home: homepage()
       );
   
   }
--- cashino/lib/pages/addpage.dart
+++ cashino/lib/pages/addpage.dart
@@ -1,95 +1,14 @@
-// ignore_for_file: prefer_const_constructors
-
-
-
-
-import 'package:cashino/utils/cropper.dart';
-import 'package:cashino/utils/picker.dart';
 import 'package:flutter/material.dart';
-import 'package:cashino/components/imagedialouge.dart';
-import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
-import 'package:image_picker/image_picker.dart';
-class addpage extends StatefulWidget {
-  final InputImage inputImage = InputImage.fromFilePath("");
-
-   addpage({super.key});
+class _addpage extends StatefulWidget {
+  const _addpage({super.key});
 
   @override
-  State<addpage> createState() => __addpageState();
+  State<_addpage> createState() => __addpageState();
 }
 
-class __addpageState extends State<addpage> {
-
-
+class __addpageState extends State<_addpage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 0, 0, 0),
-  
-      body:
-        
-           Column(
-          // ignore: prefer_const_literals_to_create_immutables
-          children:[  
-            Container(
-              height: 60,
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 55, 55, 55)
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: const Row(
-                  children: [
-                  Text("Add Transaction  ",style: TextStyle(color: Color.fromARGB(255, 192, 192, 192),fontSize: 20)),
-                     Icon(Icons.add_circle_outline,color: Color.fromARGB(255, 192, 192, 192),size: 20,),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      
-       floatingActionButton: FloatingActionButton.extended(onPressed: (){
-         imagePickerModal(context ,
-         onCameratap: (){
-          pickImage(source: ImageSource.camera).then((value) {
-            if(value!=""){
-              imageCropperPage(value, context).then((value) async {
-                  InputImage inputImage = InputImage.fromFilePath(value); 
-                    final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
-
-        RecognizedText recognizedText= await textRecognizer.processImage(inputImage);
-        print(recognizedText.text);
-
-              });
-             
-
-            
-          }});
-         },
-         onGallerytap: (){  pickImage(source: ImageSource.gallery).then((value) {
-            if(value!=""){  
-              imageCropperPage(value, context).then((value) async {
-                
-                  InputImage inputImage = InputImage.fromFilePath(value); 
-                    final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
-        try{
-        RecognizedText recognizedText= await textRecognizer.processImage(inputImage);
-        print(recognizedText.text);
-        }
-catch(e){
-  print(e);
-}
-
-              });
-            }
-          });}
-);
-       },
-       backgroundColor: Color.fromARGB(255, 101, 255, 77),
-     label:Text("Add New",style: TextStyle(color: Color.fromARGB(255, 49, 49, 49) ,),),)
-       //Icon(Icons.add,color:Color.fromARGB(255, 17, 37, 12) ,),),
-    );
-  
+    return const Placeholder();
   }
 }
